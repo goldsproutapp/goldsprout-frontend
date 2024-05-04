2ba0382a40bd8eb18c76006d08aa13e65d2d0ecb
Patrick Jones <git@patrickjones.uk>
2024-05-04 22:55:20 +0100

    Add summary of visible users to overview

--- src/views/Overview.vue
+++ src/views/Overview.vue
@@ -5,6 +5,9 @@ import type {Overview} from '@/lib/types';
 import Card from 'primevue/card';
 import {onMounted, ref} from 'vue';
 import PerformanceGraph from './performance/PerformanceGraph.vue';
+import Carousel from 'primevue/carousel';
+import {formatDecimal} from '@/lib/data';
+import Divider from 'primevue/divider';
 
 const overview = ref<Overview | null>();
 
@@ -59,20 +62,47 @@ onMounted(() => getOverview().then(res => overview.value = res));
                         </h1>
                     </template>
                 </Card>
+                <Card class="summary-card" v-if="overview.users" style="max-width: 50rem;">
+                    <template #content>
+                        <Carousel :value="Object.keys(overview.users)" :num-visible="1" circular :autoplay-interval="4000">
+                            <template #item="slotProps">
+                                <h2>{{ overview.users[slotProps.data].name }}</h2>
+                                <div class="carousel-item">
+                                    £{{ formatDecimal(overview.users[slotProps.data].total_value) }}
+                                    <Divider layout="vertical" class="hdiv" />
+                                    <Divider layout="horizontal" class="vdiv" />
+                                    <b v-if="overview.users[slotProps.data].all_time_change[0] == '-'"
+                                        style="color: var(--text-colour-negative)">
+                                        -£
+                                        {{ formatDecimal(overview.users[slotProps.data].all_time_change.slice(1)) }}
+                                    </b>
+                                    <b v-else style="color: var(--text-colour-positive)">
+                                        +£{{ formatDecimal(overview.users[slotProps.data].all_time_change) }}
+                                    </b>
+                                    <Divider layout="vertical" class="hdiv" />
+                                    <Divider layout="horizontal" class="vdiv" />
+                                    <span>{{ overview.users[slotProps.data].last_snapshot.toLocaleDateString() }}</span>
+                                </div>
+                            </template>
+                        </Carousel>
+                    </template>
+                </Card>
             </div>
             <div class="graph-container">
-                <PerformanceGraph id="" performance-type="portfolio" style="flex-grow: 1;"/>
+                <PerformanceGraph id="" performance-type="portfolio" style="flex-grow: 1;" />
             </div>
         </div>
     </div>
 </template>
 
 <style scoped>
-.container, .overview {
+.container,
+.overview {
     flex-grow: 1;
     display: flex;
     flex-direction: column;
 }
+
 .summary-cards {
     display: flex;
     flex-wrap: wrap;
@@ -82,11 +112,33 @@ onMounted(() => getOverview().then(res => overview.value = res));
     margin: var(--inline-spacing);
     min-width: 20rem;
 }
+
+.carousel-item {
+    display: flex;
+    justify-content: start;
+    font-size: x-large;
+}
+    .vdiv {
+        display: none;
+    }
+@media screen and (max-width: 650px) {
+    .carousel-item {
+        flex-direction: column;
+    }
+    .hdiv {
+        display: none;
+    }
+    .vdiv {
+        display: block;
+    }
+}
 </style>
 <style>
 .p-card-content {
     padding: 0;
+    height: 100%;
 }
+
 .graph-container {
     flex-grow: 1;
     display: flex;
