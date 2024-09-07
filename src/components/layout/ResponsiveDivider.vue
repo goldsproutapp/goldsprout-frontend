55a85ba70024f6b76c9a200722a1d48f6ddfe9ff
Patrick Jones <git@patrickjones.uk>
2024-09-07 12:27:36 +0100

    feat: improve login page

---
+++ src/components/layout/ResponsiveDivider.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import Divider from 'primevue/divider';
+import { compile, computed, type CSSProperties } from 'vue';
+const dividerStyle: CSSProperties = {
+  backgroundColor: 'var(--surface-ground)'
+};
+const props = defineProps<{
+  landscapeDirection: 'h' | 'v';
+}>();
+const classA = computed(() => `${props.landscapeDirection}div`);
+const classB = computed(() => `${props.landscapeDirection == 'h' ? 'v' : 'h'}div`);
+</script>
+
+<template>
+  <Divider layout="horizontal" :class="classA" :pt:content:style="dividerStyle" align="center"
+    ><slot
+  /></Divider>
+  <Divider layout="vertical" :class="classB" :pt:content:style="dividerStyle" align="center"
+    ><slot
+  /></Divider>
+</template>
+
+<style scoped>
+.vdiv {
+  display: none;
+}
+@media screen and (max-width: 650px) {
+  .hdiv {
+    display: none;
+  }
+  .vdiv {
+    display: flex;
+  }
+}
+</style>
--- src/views/auth/Login.vue
+++ src/views/auth/Login.vue
@@ -5,6 +5,7 @@ import { logIn } from '@/lib/auth';
 import InputText from 'primevue/inputtext';
 import Password from 'primevue/password';
 import Button from 'primevue/button';
+import ResponsiveDivider from '@/components/layout/ResponsiveDivider.vue';
 const failure = ref(false);
 const uname = ref();
 const pw = ref();
@@ -23,39 +24,93 @@ const login = async () => {
   const data = await res.json();
   logIn(data);
 };
+const demoLogin = () => {
+  uname.value = 'demo';
+  pw.value = 'demo';
+  login();
+};
 </script>
 
 <template>
-  <form class="form" @submit.prevent="login">
-    <InputText class="input" type="text" name="username" placeholder="Username" v-model="uname" />
-    <Password
-      :feedback="false"
-      class="input"
-      type="password"
-      name="password"
-      placeholder="Password"
-      v-model="pw"
-    />
-    <Button type="submit" severity="success" label="Log in" />
-    <span class="failure" v-if="failure">Incorrect username or password</span>
-    <span style="margin-top: var(--inline-spacing)" v-if="DEMO_MODE_ENABLED"
-      >To access the demo account, use username 'demo' and password 'demo'.</span
-    >
-  </form>
+  <div class="login-container">
+    <div style="flex-basis: 0; flex-grow: 2"></div>
+    <h1 class="title">Goldsprout</h1>
+    <div style="flex-basis: 0; flex-grow: 1"></div>
+    <div class="form-container">
+      <form class="form" @submit.prevent="login">
+        <InputText
+          class="input"
+          type="text"
+          name="username"
+          placeholder="Username"
+          v-model="uname"
+        />
+        <Password
+          :feedback="false"
+          class="input"
+          type="password"
+          name="password"
+          placeholder="Password"
+          v-model="pw"
+        />
+        <Button type="submit" severity="success" label="Log in" />
+        <span class="failure" v-if="failure">Incorrect username or password</span>
+      </form>
+      <template v-if="DEMO_MODE_ENABLED">
+        <ResponsiveDivider landscape-direction="v">Or</ResponsiveDivider>
+        <div class="demo-container">
+          <Button severity="success" label="Use demo account" @click="demoLogin" />
+          <span style="margin-top: var(--inline-spacing)" v-if="DEMO_MODE_ENABLED"></span>
+        </div>
+      </template>
+    </div>
+    <div style="flex-basis: 0; flex-grow: 3"></div>
+    <h1>&nbsp;</h1>
+  </div>
 </template>
 
 <style scoped>
+h1 {
+  font-size: xxx-large;
+}
+
 .form {
   display: flex;
   flex-direction: column;
   align-items: center;
 }
 .input,
-button {
+button[type='submit'] {
   margin-top: 1rem;
 }
 .failure {
   margin-top: 1rem;
   color: var(--failure-colour);
+}
+.login-container {
+  height: 100%;
+  flex-grow: 1;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+.form-container {
+  display: flex;
+  justify-content: center;
+  margin: auto;
+}
+.demo-container {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+.divider-label {
+  background-color: var(--surface-ground);
+}
+@media screen and (max-width: 650px) {
+  .form-container {
+    flex-direction: column;
+  }
 }
 </style>
