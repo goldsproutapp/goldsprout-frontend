2d459eb48a5e36076b709c86771c3d960eaea8fb
Patrick Jones <git@patrickjones.uk>
2024-09-10 15:34:15 +0100

    fix: add missing file

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,3 +1,6 @@
+import { authState } from './state';
+import type { User } from './types';
+
 export async function downloadFile(res: Response) {
   const blob = await res.blob();
   // TODO: this probably isn't spec-compliant
@@ -10,4 +13,12 @@ export async function downloadFile(res: Response) {
   a.click();
   a.remove();
   URL.revokeObjectURL(url);
+}
+
+export function hasWritePermFor(forUserID: number): boolean {
+  const user = authState.userInfo;
+  if (user.is_admin) return true;
+  if (user.access_permissions.some((perm) => perm.access_for_id == forUserID && perm.write))
+    return true;
+  return false;
 }
