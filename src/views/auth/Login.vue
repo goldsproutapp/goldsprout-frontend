0a8b5af33da45c404d8b651707483c1dce42b6b3
Patrick Jones <git@patrickjones.uk>
2024-09-21 18:54:18 +0100

    feat: add branding to login page

--- src/views/auth/Login.vue
+++ src/views/auth/Login.vue
@@ -3,9 +3,10 @@ import { ref } from 'vue';
 import { API_BASE_URL, DEMO_MODE_ENABLED } from '@/lib/constants';
 import { logIn } from '@/lib/auth';
 import InputText from 'primevue/inputtext';
-import Password from 'primevue/password';
 import Button from 'primevue/button';
 import ResponsiveDivider from '@/components/layout/ResponsiveDivider.vue';
+import IconField from 'primevue/iconfield';
+import InputIcon from 'primevue/inputicon';
 const failure = ref(false);
 const uname = ref();
 const pw = ref();
@@ -34,25 +35,26 @@ const demoLogin = () => {
 <template>
   <div class="login-container">
     <div style="flex-basis: 0; flex-grow: 2"></div>
-    <h1 class="title">Goldsprout</h1>
+    <div class="title-container">
+      <img src="/logo.png" width="50" />
+      <div class="title-text-container">
+        <h1 class="title">
+          <span>Gold</span><span style="color: var(--accent-colour)">sprout</span>
+        </h1>
+        <span class="slogan">Nurture wealth</span>
+      </div>
+    </div>
     <div style="flex-basis: 0; flex-grow: 1"></div>
     <div class="form-container">
       <form class="form" @submit.prevent="login">
-        <InputText
-          class="input"
-          type="text"
-          name="username"
-          placeholder="Username"
-          v-model="uname"
-        />
-        <Password
-          :feedback="false"
-          class="input"
-          type="password"
-          name="password"
-          placeholder="Password"
-          v-model="pw"
-        />
+        <IconField iconPosition="left" class="input">
+          <InputIcon class="pi pi-user"></InputIcon>
+          <InputText type="text" name="username" placeholder="Username" v-model="uname" />
+        </IconField>
+        <IconField iconPosition="left" class="input">
+          <InputIcon class="pi pi-lock"></InputIcon>
+          <InputText type="password" name="password" placeholder="Password" v-model="pw" />
+        </IconField>
         <Button type="submit" severity="success" label="Log in" />
         <span class="failure" v-if="failure">Incorrect username or password</span>
       </form>
@@ -72,6 +74,7 @@ const demoLogin = () => {
 <style scoped>
 h1 {
   font-size: xxx-large;
+  margin-bottom: 0;
 }
 
 .form {
@@ -112,5 +115,17 @@ button[type='submit'] {
   .form-container {
     flex-direction: column;
   }
+}
+.title-container {
+  display: flex;
+}
+.title-text-container {
+  display: flex;
+  flex-direction: column;
+  align-items: end;
+}
+.slogan {
+  font-style: italic;
+  margin-top: 5px;
 }
 </style>
